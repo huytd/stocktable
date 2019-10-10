a007065886ca05fedb1bec0193bf3f13020f4ba1
Huy Tran <kingbazoka@gmail.com>
2019-10-10 13:12:48 -0700

    Remove prototype data

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,4 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { App } from './components/App';
 
-const data = require('../data/jpm.json');
-window['data'] = data;
 render(React.createElement(App), document.getElementById("root"));
