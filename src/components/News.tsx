d025320b8e577a62cd882b2f44b89961eb5749a6
Huy Tran <kingbazoka@gmail.com>
2019-10-18 16:26:23 -0700

    Fix text encoding issue in News

--- src/components/News.tsx
+++ src/components/News.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import {useEffect, useState} from 'react';
+import {decodeHTML} from '../utils';
 
 const PARSER_URL = `https://single-rss-worker.snackylab.workers.dev/?url=`;
 const RSS_URL = `https://finance.yahoo.com/rss/headline?s=`;
@@ -34,7 +35,7 @@ export const News = (props: { symbols: string[] }) => {
       <tbody>
         {
           news.map((item, i) => <tr className="hover:bg-gray-100" key={i}>
-            <td className="p-1 text-sm"><a target="_blank" rel="noreferrer noopener" className="hover:text-gray-700" href={item.link}>{item.title}</a></td>
+            <td className="p-1 text-sm"><a target="_blank" rel="noreferrer noopener" className="hover:text-gray-700" href={item.link}>{decodeHTML(item.title)}</a></td>
             <td className="p-1 text-sm"><span className="text-gray-500">{`${item.date.toLocaleDateString()} ${item.date.toLocaleTimeString()}`}</span></td>
           </tr>)
         }
